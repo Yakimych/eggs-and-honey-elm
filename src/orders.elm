6eb2f8104d976b0a81cc5d80ec488a9f57a26704
Kyrylo Yakymenko <yakimych@gmail.com>
2018-03-11 23:46:55 +0100

    Show name and order in the list

--- src/orders.elm
+++ src/orders.elm
@@ -1,39 +1,46 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Events exposing (..)
 
+
 main : Program Never Model Msg
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
+
 type alias NewOrder =
-  { name : String
-  , order: String
-  }
+    { name : String
+    , order : String
+    }
+
 
 type alias Order =
-  { id: Int
-  , name: String
-  , order: String
-  , datePlaced: String}
+    { id : Int
+    , name : String
+    , order : String
+    , datePlaced : String
+    }
+
 
 type alias Model =
-  { newOrder : NewOrder
-  , orders : List Order 
-  }
+    { newOrder : NewOrder
+    , orders : List Order
+    }
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-  (Model { name = "TestName", order = "" } [], Cmd.none)
+    ( Model { name = "TestName", order = "" } [], Cmd.none )
 
 
 
@@ -41,18 +48,18 @@ init =
 
 
 type Msg
-  = Add NewOrder
-  | Remove Int
+    = Add NewOrder
+    | Remove Int
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Add orderToAdd ->
-      (Model model.newOrder ({ id = 1, name = orderToAdd.name, order = orderToAdd.order, datePlaced = "2018-01-01" } :: model.orders), Cmd.none)
+    case msg of
+        Add orderToAdd ->
+            ( Model model.newOrder ({ id = 1, name = orderToAdd.name, order = orderToAdd.order, datePlaced = "2018-01-01" } :: model.orders), Cmd.none )
 
-    Remove orderId ->
-        (model, Cmd.none)
+        Remove orderId ->
+            ( model, Cmd.none )
 
 
 
@@ -61,7 +68,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
@@ -70,8 +77,9 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text (model.newOrder.name) ]
-    , h2 [] [ text (toString (List.length model.orders))]
-    , button [ onClick (Add { name = "New", order = "Order" }) ] [ text "Add" ]
-    ]
+    div []
+        [ h1 [] [ text (model.newOrder.name) ]
+        , h2 [] [ text (toString (List.length model.orders)) ]
+        , ul [] (List.map (\o -> li [] [ text (o.name ++ " " ++ o.order) ]) model.orders)
+        , button [ onClick (Add { name = "New", order = "Order" }) ] [ text "Add" ]
+        ]
