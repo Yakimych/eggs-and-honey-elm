7cc472873e445bab6f175664419ea533fde3cbbb
Kyrylo Yakymenko <yakimych@gmail.com>
2018-03-31 14:40:49 +0200

    Add basic validation for name and order

--- src/orders.elm
+++ src/orders.elm
@@ -19,24 +19,52 @@ main =
 -- MODEL
 
 
+type OrderType
+    = Eggs
+    | Honey
+
+
+orderToString : OrderType -> String
+orderToString order =
+    case order of
+        Eggs ->
+            "Eggs"
+
+        Honey ->
+            "Honey"
+
+
+maybeOrderFromString : String -> Maybe OrderType
+maybeOrderFromString orderString =
+    case orderString of
+        "Eggs" ->
+            Just Eggs
+
+        "Honey" ->
+            Just Honey
+
+        _ ->
+            Nothing
+
+
 type alias Order =
     { id : Int
     , name : String
-    , order : String
+    , order : OrderType
     , datePlaced : String
     }
 
 
 type alias Model =
     { newName : String
-    , newOrder : String
+    , newOrder : Maybe OrderType
     , orders : List Order
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" "" [], Cmd.none )
+    ( Model "" Nothing [], Cmd.none )
 
 
 
@@ -50,11 +78,49 @@ type Msg
     | AddedOrder String
 
 
+type Either a b
+    = Left a
+    | Right b
+
+
+toBool : Either a b -> Bool
+toBool eitherValue =
+    case eitherValue of
+        Left _ ->
+            True
+
+        Right _ ->
+            False
+
+
+validateNewOrder : String -> Maybe a -> Either ( String, a ) ()
+validateNewOrder name order =
+    case ( name, order ) of
+        ( "", _ ) ->
+            Right ()
+
+        ( _, Nothing ) ->
+            Right ()
+
+        ( name, Just order ) ->
+            Left ( name, order )
+
+
+canAddOrder : String -> Maybe a -> Bool
+canAddOrder name order =
+    validateNewOrder name order |> toBool
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Add ->
-            ( Model "" model.newOrder ({ id = 1, name = model.newName, order = model.newOrder, datePlaced = "2018-01-01" } :: model.orders), Cmd.none )
+            case validateNewOrder model.newName model.newOrder of
+                Right () ->
+                    ( model, Cmd.none )
+
+                Left ( newName, newOrder ) ->
+                    ( Model "" model.newOrder ({ id = 1, name = model.newName, order = newOrder, datePlaced = "2018-01-01" } :: model.orders), Cmd.none )
 
         Remove orderId ->
             ( model, Cmd.none )
@@ -63,7 +129,7 @@ update msg model =
             ( { model | newName = addedName }, Cmd.none )
 
         AddedOrder addedOrder ->
-            ( { model | newOrder = addedOrder }, Cmd.none )
+            ( { model | newOrder = maybeOrderFromString addedOrder }, Cmd.none )
 
 
 
@@ -82,10 +148,9 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (model.newOrder) ]
-        , h2 [] [ text (toString (List.length model.orders)) ]
-        , ul [] (List.map (\o -> li [] [ text (o.name ++ " " ++ o.order) ]) model.orders)
+        [ h2 [] [ text (toString (List.length model.orders)) ]
+        , ul [] (List.map (\o -> li [] [ text (o.name ++ " " ++ (orderToString o.order)) ]) model.orders)
         , input [ type_ "text", placeholder "Name", value model.newName, onInput AddedName ] []
         , input [ type_ "text", placeholder "Order", onInput AddedOrder ] []
-        , button [ onClick Add ] [ text "Add" ]
+        , button [ onClick Add, disabled (not <| canAddOrder model.newName model.newOrder) ] [ text "Add" ]
         ]
